f78c5127a319270c22d20f256c6ffabbd6478535
deinVater94 <123596605+deinVater94@users.noreply.github.com>
2026-08-27 22:23:41 +0200

    Add files via upload

--- Models/LauncherSettings.cs
+++ Models/LauncherSettings.cs
@@ -3,8 +3,8 @@ namespace LeipzigCraft.Launcher.Models;
 public static class LauncherSettings
 {
     public const string MinecraftVersion = "1.21";
-    public const string FabricLoaderVersion = "0.18.4";
-    public const string FabricVersionId = "fabric-loader-0.18.4-1.21";
+    public const string FabricLoaderVersion = "0.17.3";
+    public const string FabricVersionId = "fabric-loader-0.17.3-1.21";
 
     public const string PackManifestUrl =
         "https://leipzigcraft.com/launcher/pack.json";

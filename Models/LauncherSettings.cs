162e1bcb143cd5dd7e22fda866754c0e0e2f387a
deinVater94 <123596605+deinVater94@users.noreply.github.com>
2026-08-27 21:22:34 +0200

    Add files via upload

--- Models/LauncherSettings.cs
+++ Models/LauncherSettings.cs
@@ -6,11 +6,13 @@ public static class LauncherSettings
     public const string FabricLoaderVersion = "0.18.4";
     public const string FabricVersionId = "fabric-loader-0.18.4-1.21";
 
-    // Small JSON file hosted by the LeipzigCraft website.
-    public const string PackManifestUrl = "https://leipzigcraft.com/launcher/pack.json";
+    public const string PackManifestUrl =
+        "https://leipzigcraft.com/launcher/pack.json";
+
+    public const string PackSignatureUrl =
+        "https://leipzigcraft.com/launcher/pack.sig";
 
     public const int MaximumRamMb = 6144;
 
-    // Fill this in later if you want one-click direct server joining.
     public const string ServerAddress = "";
 }
